4a9042cec963ef013925f3fc6bd4cc13ce211d81
Matthes342 <bblasiuss@gmail.com>
2021-04-02 23:41:35 -0700

    calculated temp per location and keytime and added download for date range

--- Webapp/dashserver.py
+++ Webapp/dashserver.py
@@ -41,7 +41,12 @@ df2 = pd.read_csv("CurrentBaseline.csv")
 #df2 = df.groupby(["Location", "CellNo"], as_index=False).mean() <- made by 6 month data using this call (then do df to csv)
 df2.columns = ["Location", "CellNo","VoltMean", "ResistMean", "TempMean"]
 
-result = pd.merge(df1, df2, on=["Location","CellNo"])
+df3 = df1.groupby(["KeyTime","Location"], as_index=False).mean()
+#calculates temp per location and keytime
+df3=df3.rename(columns = {'TempValue':'TempValuetest'})
+
+#result = pd.merge(df1, df2 on=["Location","CellNo"])
+result = df1.merge(df2[["Location", "CellNo","ResistMean"]],on=["Location","CellNo"]).merge(df3[["KeyTime","Location","TempValuetest"]],on=["KeyTime","Location"])
 
 #all normal tags are blue, I separated all different tags into columns to avoid stacking errors (this could be useful?)
 result["Tag1"] = "blue"
@@ -58,6 +63,12 @@ result.loc[(result["AmbientTemp"] > 30), "Tag2"] = "red"
 result.loc[(result["TempValue"] > 3+result["AmbientTemp"]), "Tag3"] = "yellow"
 #tag 4 (idk what ripple current is yet)
 #result.at[i, "VoltValue"]/result.at[i, "ResistValue"] > .0005*result.at[i, "TotalCurrent"]:
+
+#put this in separate table
+#track ambient temp over 25 by how much and for how long
+#gaps are not expected
+
+#calculate tempmean per keytime and Location
 
 #structure for iterating through dataframe with for loop (alot slower runtime)
 #for i in result.index:
@@ -79,11 +90,6 @@ app = dash.Dash(__name__, prevent_initial_callbacks=True)
 app.layout = html.Div([
     html.Div(
     children=[html.H2("Bart Battery Viewer")],
-    style=dict(display='flex', justifyContent='center')
-    ),
-    html.Div(
-    children=[html.Button("Download csv", id="btn"), 
-    Download(id="download"),],
     style=dict(display='flex', justifyContent='center')
     ),
     html.Div(className = "row", children=[
@@ -118,6 +124,35 @@ app.layout = html.Div([
         step=1,
     ),
     ]),
+    html.Div(children=[
+        html.Div(
+        children=[
+        html.Label(['Select Data Range to Download:'], style={'font-weight': 'bold', "text-align": "center"}),
+        dcc.DatePickerRange(
+        id = 'Time',
+        display_format='M-D-Y',
+        start_date_placeholder_text="Start Date",
+        end_date_placeholder_text="End Date",
+        minimum_nights=0,
+        #has to update with n intervals
+        min_date_allowed=df.KeyTime.min(),
+        max_date_allowed=df.KeyTime.max(),
+        clearable=True,
+        ),
+        #can use this or the date range (ask mentors next meeting)
+        dcc.Dropdown(
+            id = 'times', 
+            options=[{'label': i, 'value': i} for i in df.KeyTime.dt.date.unique()]),
+        ],
+        style=dict(width='50%')
+        ),
+        #html.Div(
+        #children=[html.Button("Download csv", id="btn"), 
+        Download(id="download"),
+        #],
+        #style=dict(width='50%', display='flex')
+        #),
+    ],),
     dcc.Graph(id="graph"),
     html.H2("Errors/Outliers"),
     dash_table.DataTable(
@@ -166,23 +201,32 @@ def update_graph(Locname, yaxisname, Tag, srange):
     return fig
 
 @app.callback(Output("download", "data"), 
-    [Input("btn", "n_clicks")])
-def generate_csv(n_nlicks):
+    [
+    #Input("btn", "n_clicks_timestamp"), scrapping download button for now
+    Input('Time', 'start_date'),
+    Input('Time', 'end_date'),
+    Input("Loc", "value"),
+    ])
+def generate_csv(start, end, Locname):
+    if start is not None and end is not None and Locname is not None:
         df = getdb(conn,cur)
+        df = df[(df["Location"] == Locname) & (df["KeyTime"] >= start) & (df["KeyTime"] <= end)]
         #can do stuff  to dataframe here
         return send_data_frame(df.to_csv, filename="battdata.csv")
 
 @app.callback([Output('rangeslider', 'max')],
               [Input('Loc', 'value')])
 def update_slider(Locname):
-    df = getdb(conn,cur)
-    df = df[df["Location"] == Locname]
-    max = round(int(df["CellNo"].max())),
-    return max
+    if Locname is not None:
+        df = getdb(conn,cur)
+        df = df[df["Location"] == Locname]
+        max = round(int(df["CellNo"].max())),
+        return max
+    return [0]
 
 @app.callback(Output('range-slider-label', 'children'),
     [Input('rangeslider', 'value')])
 def update_output(value):
     return 'Cells {} are selected for viewing'.format(value)
 
-app.run_server(debug=True)
+app.run_server(debug=False)
